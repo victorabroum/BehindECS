48f9937d3598b8780d813dc7b084234b17acc444
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2025-02-19 14:58:38 -0400

    feat: Add entityManager on BBaseSystem

--- Sources/BehindECS/System/BBaseSystem.swift
+++ Sources/BehindECS/System/BBaseSystem.swift
@@ -9,6 +9,12 @@
 import MetalKit
 
 open class BBaseSystem: BSystem {
+    public var entityManager: BEntityManager
+    
+    public init(entityManager: BEntityManager) {
+        self.entityManager = entityManager
+    }
+    
     open func render(commandEncoder: any MTLRenderCommandEncoder, deltaTime: Float) {
     
     }
